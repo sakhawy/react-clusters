9a3ea0cc84c1e1b2ef457c22ea45993087f27b2d
sakhawy <50373064+mo3ist@users.noreply.github.com>
2022-02-11 16:46:30 +0200

    feat: add ad Marker icon

--- src/components/ClusteredMap.jsx
+++ src/components/ClusteredMap.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 import './ClusteredMap.css'
+import { icon } from "../utils/icons";
 
 const ClusteredMap = () => {
 	return (
@@ -11,7 +12,10 @@ const ClusteredMap = () => {
 					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 				/>
-				<Marker position={[51.505, -0.09]}>
+				<Marker  
+					position={[51.505, -0.09]}
+					icon={ icon }	
+				>
 					<Popup>
 					A pretty CSS3 popup. <br /> Easily customizable.
 					</Popup>
